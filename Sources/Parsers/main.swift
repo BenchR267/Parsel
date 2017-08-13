9d7bfcfb73e2a36600bd9db3330c45a567987aed
Benjamin Herzog <mail@benchr.de>
2017-08-13 15:33:45 +0200

    Added temporary executable
    only for testing during first development

---
+++ Sources/Parsers/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ParsersPackageDescription
+//
+//  Created by Benjamin Herzog on 13.08.17.
+//
+
+import Foundation
+
+// This file is temporary during first development
+
+print("Hello world")
